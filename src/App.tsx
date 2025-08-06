f57104725293e1a20fd9761075d747607b89bf93
unknown <everythingfree4200@gmail.com>
2025-08-07 02:11:55 +0530

    02345

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { App } from './App';
 import './index.css';
 import { initializeAuth } from './api';
 import { SpeechSynthesizer } from './utils/speech';
